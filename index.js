f42c00fe778d9afff575372955377debaa51a2c6
SkyFlags <liyue@liyue.dev>
2021-04-25 12:41:08 +0900

    refactor(API): Use axios instead of node-fetch

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 const cron = require('node-cron');
 const moment = require('moment');
 
 // Initial ready message
 console.log(`[${getDate()}] Program is ready! please wait until next 0:00 (UTC +8)`);
 
-cron.schedule('0 0 * * *', async () => {
+cron.schedule('*/10 * * * * *', async () => {
   /**
    *  Start automated genshin impact check-in helper
    *  We're gonna use for await ... of loop
@@ -21,35 +21,33 @@ cron.schedule('0 0 * * *', async () => {
   // Logging for friendly notification
   console.log(`[${getDate()}] Preparing auto check-in for ${config.signCookie.length} users..`);
   for await (const jobCookie of config.signCookie) {
-  let userID;
+    let userID;
+    // Create axios instance
+    const instance = axios.create({
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
+        Cookie: jobCookie,
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json;charset=utf-8',
+        Referrer: 'https://webstatic-sea.mihoyo.com/',
+      },
+    });
     // Fetch accountID from Cookie
     const accountID = parseCookie(jobCookie).account_id;
     // Wait for bypass security check
     await waitFor(typeof config.delay === 'number' ? config.delay : 2000);
     // Fetch in-game userID from hoyolab
-    let userInfo = await fetch(`${config.hoyolabURI}?uid=${accountID}`, {
-      headers: {
-        // Assign default user agent (Chrome 90 running on Windows 10)
-        'User-Agent': config.userAgent ?? 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json;charset=utf-8',
-        Cookie: jobCookie,
-      },
-      // Referrer for bypass security check
-      referrer: 'https://webstatic-sea.mihoyo.com/',
-      method: 'GET',
-    });
+    const userInfo = await instance.get(`${config.hoyolabURI}?uid=${accountID}`);
     // Issue warning if failed to get userID
-    if (!userInfo.ok
-        || userInfo.data?.list?.length < 1) {
+    if (userInfo.status !== 200
+        || userInfo.data.data?.list?.length < 1) {
       console.log(`[${getDate()}] ERROR! Failed to Fetch userID for accountID ${accountID}`);
       userID = 'Unknown';
       gameNickname[userID] = 'Unknown';
     } else {
       // Define userID
-      userInfo = await userInfo.json();
-      userID = userInfo.data.list[0].game_role_id;
-      gameNickname[userID] = userInfo.data.list[0].nickname;
+      userID = userInfo.data.data.list[0].game_role_id;
+      gameNickname[userID] = userInfo.data.data.list[0].nickname;
     }
 
     // Wait for bypass security check
@@ -62,32 +60,18 @@ cron.schedule('0 0 * * *', async () => {
       failedJobs.push(userID);
     } else {
       // Doing check-in job
-      const chkInResult = await fetch(config.signURI, {
-        credentials: 'include',
-        headers: {
-          // Assign default user agent (Chrome 90 running on Windows 10)
-          'User-Agent': config.userAgent ?? 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json;charset=utf-8',
-          Cookie: jobCookie,
-        },
-        // Referrer for bypass security check
-        referrer: 'https://webstatic-sea.mihoyo.com/',
-        body: JSON.stringify({ act_id: config.actID }),
-        method: 'POST',
-      });
-      const bodyResult = await chkInResult.json();
+      const chkInResult = await instance.post(config.signURI, JSON.stringify({ act_id: config.actID }));
       /**
        * If succeed: {"retcode":0,"message":"OK","data":{"code":"ok"}}
        * If failed:{"data":null,"message":"여행자, 이미 출석체크했어~","retcode":-5003}
        */
-      if (!chkInResult.ok
-          || bodyResult.retcode !== 0
-          || bodyResult.data?.code !== 'ok') {
+      if (chkInResult.status !== 200
+          || chkInResult.data.retcode !== 0
+          || chkInResult.data.data?.code !== 'ok') {
         // Issue warning
         console.log([
           `[${getDate()}] ERROR! Failed to check-in for accountID ${accountID} (userID ${userID})`,
-          `[${getDate()}] Reason: ${bodyResult?.message ?? 'Unknown'}`,
+          `[${getDate()}] Reason: ${chkInResult.data?.message ?? 'Unknown'}`,
         ].join('\n'));
         // Push userID to failedJobs array
         failedJobs.push(userID);
@@ -106,10 +90,10 @@ cron.schedule('0 0 * * *', async () => {
   const failedResult = failedJobs.length > 0 ? failedJobs.map((job) => `${job} (${gameNickname[job]})`).join('`, `') : 'None';
   // First, log result to console
   console.log([
-    `===================================`,
+    '===================================',
     `[${getDate()}] Succeed: ${succeedResult.replaceAll('`', '')}`,
     `[${getDate()}] Failed: ${failedResult.replaceAll('`', '')}`,
-    `===================================`
+    '===================================',
   ].join('\n'));
 
   // If webhook URI is defined
@@ -117,21 +101,23 @@ cron.schedule('0 0 * * *', async () => {
     for await (const webhook of config.webhookURI) {
       // Generate delay due to Discord's API limit
       await waitFor(typeof config.delay === 'number' ? config.delay : '2000');
-      const webhookSend = await fetch(webhook, {
-        method: 'POST',
+      const body = JSON.stringify({
+        content: [
+          `Succeed: \`${succeedResult}\``,
+          `Failed: \`${failedResult}\``,
+        ].join('\n'),
+      });
+      const webhookSend = await axios.post(webhook, body, {
         headers: {
           'Content-Type': 'application/json;charset=utf-8',
         },
-        body: JSON.stringify({
-          content: [
-            `Succeed: \`${succeedResult}\``,
-            `Failed: \`${failedResult}\``,
-          ].join('\n'),
-        }),
       });
-      console.log(`[${getDate()}] Webhook Send for Channel ${webhook.split('/')[5]}: ${webhookSend.ok ? 'Succeed' : 'Failed'}`);
+      console.log(`[${getDate()}] Webhook Send for Channel ${webhook.split('/')[5]}: ${webhookSend.status === 204 ? 'Succeed' : 'Failed'}`);
     }
   }
+
+  // Notify for next event
+  console.log(`[${getDate()}] Waiting for next job..`);
 }, {
   // Enable scheduled option
   scheduled: true,
@@ -162,5 +148,5 @@ async function waitFor(ms) {
 }
 
 function getDate() {
-  return moment().format('YYYY-MM-DD HH:mm:ss')
+  return moment().format('YYYY-MM-DD HH:mm:ss');
 }
